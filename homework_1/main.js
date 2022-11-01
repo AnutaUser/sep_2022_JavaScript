8112926ffb22c214ba23ad052fa1e8a894b0f2f8
AnutaUser <bezkorovainaanna048@gmail.com>
2022-11-01 15:03:24 +0200

    homework_1: variables, typeof, prompt

---
+++ homework_1/main.js
@@ -0,0 +1,106 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за
+// допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let hello = 'Hello';
+// console.log(hello);
+// document.write(hello);
+//
+// let owu = 'owu';
+// console.log(owu);
+// document.write(owu);
+//
+// let com = 'com';
+// console.log(com);
+// document.write(com);
+//
+// let ua = 'ua';
+// console.log(ua);
+// document.write(ua);
+//
+// console.log(hello + owu + com + '.' + ua);
+// document.write(hello + owu + com + '.' + ua);
+//
+// let one = 1;
+// console.log(one);
+// document.write(one);
+//
+// let ten = 10;
+// console.log(ten);
+// document.write(ten);
+//
+// let nnn = '-999';
+// console.log(nnn);
+// document.write(nnn);
+//
+// let ott = 123;
+// console.log(ott);
+// document.write(ott);
+//
+// let pi = 3.14;
+// console.log(pi);
+// document.write(pi);
+//
+// let e = 2.16;
+// console.log(e);
+// document.write(e);
+//
+// let bool_true = true;
+// console.log(bool_true);
+// document.write(bool_true);
+//
+// let bool_false = false;
+// console.log(bool_false);
+// document.write(bool_false);
+
+// let firstName = 'Bezkorovaina';
+// let middleName = 'Anna';
+// let lastName = 'Volodymyrivna';
+// // let person = firstName + middleName + lastName;
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+//
+// console.log(firstName, middleName, lastName);
+// document.write(`${firstName} ${middleName} ${lastName}`);
+//
+// console.log(firstName +' ' + middleName + ' ' + lastName);
+// document.write(firstName +' ' + middleName + ' ' + lastName);
+//
+// console.log(person);
+// document.write(person);
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// console.log(prompt('Please, enter your first name!'));
+// console.log(prompt('Please, enter your second name!'));
+// console.log(prompt('Please, enter your age!'));
+
+// document.write(prompt('Please, enter your first name!') + ' ');
+// document.write(prompt('Please, enter your second name!') + ' ');
+// document.write(prompt('Please, enter your age!'));
+
+// let arr = [
+//     {name: 'Vasya', age: 25, status: false, wife: {name: 'Ira', age: 22}},
+//     {name: 'Igor', age: 32, status: true, wife: {name: 'Olya', age: 28}},
+//     {name: 'Oleg', age: 45, status: true},
+// ];
+//
+// console.log(arr);
+// console.log(arr[0].name);
+// console.log(arr[0].wife);
+// console.log(arr[0].wife.name);
+// document.write(arr[0].name);
+// document.write(arr[0].wife.name, arr[0].name);
