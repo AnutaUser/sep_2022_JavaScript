459656eb81b33b4eb50359b32f7fd7811468c840
AnutaUser <bezkorovainaanna048@gmail.com>
2022-11-10 14:14:29 +0200

    homework_4_loops:_for_while

--- homework_4_cycles/main.js
+++ homework_4_cycles/main.js
@@ -113,17 +113,17 @@
 // --------------------
 //     є масив
 let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
+    {name: 'Vasya', age: 31, status: false},
+    {name: 'Petya', age: 30, status: true},
+    {name: 'Kolya', age: 29, status: true},
+    {name: 'Olya', age: 28, status: false},
+    {name: 'Max', age: 30, status: true},
+    {name: 'Anya', age: 31, status: false},
+    {name: 'Oleg', age: 28, status: false},
+    {name: 'Andrey', age: 29, status: true},
+    {name: 'Masha', age: 30, status: true},
+    {name: 'Olya', age: 31, status: false},
+    {name: 'Max', age: 31, status: true}
 ];
 // за допомоги циклу вивести:
 // - користувачів зі статусом true
